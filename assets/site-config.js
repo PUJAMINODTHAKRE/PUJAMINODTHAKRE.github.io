97008d5401c8404c156337bc34368571090e7ef9
Puja Minodji Thakre <142625953+PUJAMINODTHAKRE@users.noreply.github.com>
2026-09-20 12:23:51 +0530

    Delete assets directory

--- assets/site-config.js
+++
@@ -1,6 +0,0 @@
-window.PUJA_SITE = {
-  name: 'Puja Minodji Thakre',
-  email: 'poojathakre33@gmail.com',
-  github: 'https://github.com/PUJAMINODTHAKRE/healthcare-kb-representation-evaluation',
-  doi: 'https://doi.org/10.1007/978-3-032-24807-7_4'
-};
